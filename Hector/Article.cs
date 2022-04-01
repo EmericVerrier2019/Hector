0aadd828a9e7c3d121f7a2a31cb90b0842cffe1b
Robin Lejeune <robin.lyceemc@gmail.com>
2022-04-01 14:23:38 +0200

    Création classe BDD

--- Hector/Article.cs
+++ Hector/Article.cs
@@ -8,6 +8,21 @@ namespace Hector
 {
     internal class Article
     {
-        private int test;
+        private String RefArticle;
+        private String Description;
+        private SousFamille RefSousFamille;
+        private Marque RefMarque;
+        private int PrixHT;
+        private int Quantite;
+
+        public Article (String RefArticle, String Description, SousFamille RefSousFamille, Marque RefMarque, int PrixHT, int Quantite)
+        {
+            this.RefArticle = RefArticle;
+            this.Description = Description;
+            this.RefSousFamille = RefSousFamille;
+            this.RefMarque = RefMarque;
+            this.PrixHT = PrixHT;
+            this.Quantite = Quantite;
+        }
     }
 }
